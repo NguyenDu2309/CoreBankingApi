a872ff8614b85c899a28bff610741338b4cc63ca
Nguyen Du <nguyendu2004.anhuu@gmail.com>
2025-10-23 22:01:36 +0700

    Định nghĩa các model Entity Framework (DONE)

---
+++ CoreBanking.Infrastructure/Data/CoreBankingContextFactory.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace CoreBanking.Infrastructure.Data;
+public class CoreBankingDbContextFactory : IDesignTimeDbContextFactory<CoreBankingDbContext>
+{
+    public CoreBankingDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<CoreBankingDbContext>();
+        object value = optionsBuilder.UseNpgsql("Host=localhost;Database=CoreBankingDb;Username=postgres;Password=yourpassword");
+        return new CoreBankingDbContext(optionsBuilder.Options);
+    }
+}
