f1297ee631bb9812f6882fdee7e7046d87dd1a5d
Nguyen Du <nguyendu2004.anhuu@gmail.com>
2025-10-29 22:16:13 +0700

    rename AppHost

--- CoreBanking.AppHost/CoreBanking.AppHost.AppHost/obj/Debug/net9.0/Aspire/references/_AppHost.ProjectMetadata.g.cs
+++ CoreBanking.AppHost/CoreBanking.AppHost.AppHost/obj/Debug/net9.0/Aspire/references/_AppHost.ProjectMetadata.g.cs
@@ -5,8 +5,8 @@ namespace Projects;
 [global::System.CodeDom.Compiler.GeneratedCode("Aspire.Hosting", null)]
 [global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage(Justification = "Generated code.")]
 [global::System.Diagnostics.DebuggerDisplay("Type = {GetType().Name,nq}, ProjectPath = {ProjectPath}")]
-public class CoreBanking_AppHost_AppHost
+public class CoreBanking_AppHost
 {
-    private CoreBanking_AppHost_AppHost() { }
+    private CoreBanking_AppHost() { }
     public static string ProjectPath => """C:\Users\Du\CoreBanking.NET\CoreBanking.AppHost\CoreBanking.AppHost.AppHost""";
 }
